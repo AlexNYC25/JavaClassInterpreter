5091289649dcf0c75412b3436dfc331b596a0c34
Alexis Montes <montes.alexis25@gmail.com>
2020-11-04 23:44:14 -0500

    added initlal classes

---
+++ Obj.java
@@ -0,0 +1,12 @@
+abstract class Obj
+{
+	boolean visited = false; // indicates if this object has been visited by traverse() function
+
+	Obj()
+
+	// This constructor will be invoked every time the constructor of any descendant class is invoked.
+
+	{
+
+	}
+}
